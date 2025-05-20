f823a2baa6ec60e8a205a8ee7150c31b0003a763
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-20 16:44:46 +0600

    added recipe collection and applied post method

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zc7c13h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,28 +16,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // collection
+    const RecipeCollection = client.db("recipeBookDB").collection("recipes");
+
+    
+    app.post('/recipes', async(req, res) =>{
+      const newRecipe = req.body;
+      const result = await RecipeCollection.insertOne(newRecipe);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Recipe Book App server is working");
+});
 
-app.get('/', (req, res) =>{
-    res.send('Recipe Book App server is working');
-})
-
-app.listen(port, () =>{
-    console.log('Recipe Book App server is working');
-})
+app.listen(port, () => {
+  console.log("Recipe Book App server is working");
+});
