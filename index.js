f713416d6ea286b04d82e88a87e64f33f52561f6
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-22 00:34:21 +0600

    added delete method

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/recipe/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await RecipeCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
